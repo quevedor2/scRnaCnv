c9a1dbc14e2b6cc7f56b65ca36b6e0f312021f3c
Rene Quevedo <rquevedo@uhnresearch.ca>
2021-03-21 16:49:16 -0400

    Initial commit of a blank R project with DESCRIPTION

---
+++ build/build.R
@@ -0,0 +1,30 @@
+#install.packages("devtools")
+library(devtools)
+
+pkg=file.path("~/git", "scRnaCnv")
+setwd(pkg)
+
+
+#use_description()
+
+#### Assembling data ####
+usethis::use_testthat()               # unit testing
+usethis::use_vignette("vignette")  # vignette documentation
+usethis::use_data_raw()               # raw data
+
+usethis::use_package("assertthat")
+usethis::use_package("utils")
+usethis::use_package("stats")
+usethis::use_package("dplyr")
+
+
+
+#### Building ####
+devtools::load_all()
+devtools::document(pkg)
+devtools::check(pkg)
+devtools::build_vignettes(pkg)
+
+devtools::build(pkg)
+devtools::install(pkg)
+devtools::install_github("quevedor2/scRnaCnv", ref = "dev")
---
+++ tests/testthat.R
@@ -0,0 +1,4 @@
+library(testthat)
+library(scRnaCnv)
+
+test_check("scRnaCnv")
